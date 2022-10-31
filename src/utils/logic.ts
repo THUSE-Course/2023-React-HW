2f8e998f9661def6b425f7875e3fd3e0ca714f11
Ashitemaru <qhd19@mails.tsinghua.edu.cn>
2022-10-31 18:46:46 +0800

    Feature: Add Step 1

---
+++ src/utils/logic.ts
@@ -0,0 +1,16 @@
+export const BOARD_LENGTH = 100;
+export type Board = (0 | 1)[][];
+
+export const stepBoard = (board: Board): Board => {
+    const newBoard: Board = [];
+
+    /**
+     * @todo 请在下面两条注释之间的区域填写你的代码完成该游戏的核心逻辑
+     * @note 你可以使用命令 yarn test 来运行我们编写的单元测试与我们提供的参考实现对拍
+     */
+    // Step 1 BEGIN
+
+    // Step 1 END
+
+    return newBoard;
+};
